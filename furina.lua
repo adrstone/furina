47e0bf51f808073105506aaa876429abc7bc03e6
sangjoestar <lethanhnamnd2021@gmail.com>
2025-04-30 14:46:51 +0700

    Update furina.lua

--- furina.lua
+++ furina.lua
@@ -7,7 +7,7 @@ dsc.gg/hydrahub  |   31.01 - added Show_Assets toggle. Soon ill make normal bypa
 
 --- FLUENT PLUS SETTINGS ---
 local Show_Button = true -- Shows the button for toggle fluent ui manually. If "false", works only on mobile, if "true", works everytime.
-local Button_Icon = "http://www.roblox.com/asset/?id=140448631097027" -- Icon of the button for toggle fluent ui
+local Button_Icon = "http://www.roblox.com/asset/?id=108593743519357" -- Icon of the button for toggle fluent ui
 ----------------------------
 
 local Lighting = game:GetService("Lighting")
